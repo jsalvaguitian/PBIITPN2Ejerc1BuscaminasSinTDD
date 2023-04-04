efa46c716de7f6ebd22409d092915e6d99fb20df
jsalvaguitian <jsalvaguitian@frba.utn.edu.ar>
2023-04-04 18:31:19 -0300

    buscaminas sin tdd y sin casilleros liberados aleatoriamente

---
+++ src/ar/edu/unlam/pb2/Buscaminas.java
@@ -0,0 +1,220 @@
+package ar.edu.unlam.pb2;
+
+
+public class Buscaminas {
+	
+	private char[][] tableroFront;
+	private boolean[][] tableroBack;
+	private Integer filas;
+	private Integer columnas;
+	private Integer minas;
+	private final int MIN_CANT_CASILLEROS_LIBERADOS;
+	private int cantidadDeCasillerosLiberados;
+
+	public Buscaminas(Nivel nivel) { // ¿mmm.. Aqui podré arrojar una excepcion para que se encargue el main? :-s
+		this.filas = nivel.getFilas();
+		this.columnas = nivel.getColumnas();
+		this.minas = nivel.getMinas();
+		this.tableroBack = new boolean[filas][columnas];
+		this.tableroFront = new char[filas][columnas];
+		this.cantidadDeCasillerosLiberados = 0;
+		this.MIN_CANT_CASILLEROS_LIBERADOS = nivel.getCantidadMinimaDeCasillerosLiberados();
+	}
+
+	
+	
+	public String mostrarTableroFront(boolean estaGanando, int fila, int columna) {
+		String muestra ="";
+		
+		for(int i = 0; i<filas; i++) {
+			muestra += i;
+			
+			if(i<10) 
+				muestra += " ";
+
+			for(int j =0; j<columnas; j++) {
+				if(!estaGanando &&  this.tableroBack[i][j]== true) {
+					if(i==fila && j==columna) {
+						this.tableroFront[i][j]='#';
+					}else
+						this.tableroFront[i][j]='*';
+				}
+
+				if(this.tableroFront[i][j]=='\0') {
+					muestra += "[ ]";
+				}else {
+					muestra += "[" + tableroFront[i][j] + "]";
+				}
+			}
+			muestra +="\n";
+		}
+		return muestra;
+	}
+	
+	
+	
+	public String mostrarTableroFront() {
+		String tableroInicial= "";
+		
+		for(int i = 0; i<filas; i++) {
+			tableroInicial+= i;
+			if(i<10) 
+				tableroInicial += " ";
+			for(int j =0; j<columnas; j++) {
+				if(this.tableroFront[i][j]=='\0') {
+					tableroInicial += "[ ]";
+				}
+			}
+			tableroInicial +="\n";
+		}
+		return tableroInicial;
+	}
+	
+	
+	
+	public String mostrarTableroBack() {
+		String muestra = "";
+		
+		for(int i=0; i<filas; i++) {
+			for(int j=0; j<columnas; j++) {
+				if(this.tableroBack[i][j]==false) {
+					muestra += "[ ]";
+				}else {
+					muestra += "[*]";
+				}
+			}
+			muestra +="\n";
+		}
+		
+		return muestra;
+	}
+	
+	
+
+	public void generarLosCasillerosConMinas(int fila, int columna) {
+		int posicionI, posicionJ;
+		if(this.validarFilaColumna(fila, columna)) { 
+			for (int i = 1; i <= this.minas; i++) {
+				do {
+					posicionI = (int)(Math.random() * this.filas); //0-7
+					posicionJ = (int)(Math.random() * this.columnas);
+					
+				} while ((posicionI == fila && posicionJ == columna)|| this.tableroBack[posicionI][posicionJ]== true);// chequear que la bombita no esté donde esta el jugador, o que no se repita
+				
+				this.tableroBack[posicionI][posicionJ] = true; //en ese casillero ponele la bombita
+			}
+		}
+	}
+
+
+	public boolean validarFilaColumna(int fila, int columna) {
+		if((fila>=0 && fila<this.filas) && (columna>=0 && columna<this.columnas))
+			return true;
+		else
+			return false;	
+	}
+
+	
+	
+	public void guardarLaCantidadDeBombasQueHayAlrededor(int fila, int columna) {
+		int contadorDeMinas =0;
+		if(fila == 0 && columna == 0) {
+			for(int i = fila; i<= fila +1; i++) {
+				for(int j= columna; j<= columna +1; j++) {
+					if(this.tableroBack[i][j]== true) {
+						contadorDeMinas++;			
+					}	
+				}
+			}
+			
+		}
+		if(fila == this.filas-1 && columna == 0) {
+			for(int i=fila-1; i<=fila; i++) {
+				for(int j= columna; j<=columna+1; j++) {
+					if(this.tableroBack[i][j]== true) {
+						contadorDeMinas++;	
+					}
+				}	
+			}
+			
+		}
+		if(fila == 0 && columna == this.columnas-1) {
+			for(int i= fila; i<=fila+1; i++){
+				for(int j= columna-1; j<=columna; j++) {
+					if(this.tableroBack[i][j]==true)
+						contadorDeMinas++;		
+				}
+			}
+			
+		}
+		if(fila == this.filas-1 && columna == this.columnas-1) {
+			for(int i=fila-1; i<=fila; i++) {
+				for(int j= columna-1; j<=columna; j++) {
+					if(this.tableroBack[i][j]== true) {
+						contadorDeMinas++;
+					}
+					
+				}
+				
+			}
+		}
+		if(columna == 0 && (fila>0 && fila<this.filas-1)) {
+			for(int i= fila-1; i<=fila+1; i++) {
+				for(int j= columna; j<=columna+1; j++) {
+					if(this.tableroBack[i][j]== true)
+						contadorDeMinas++;
+				}
+			}
+		}
+		if(columna==this.columnas-1 && (fila>0 && fila < this.filas-1)) {
+			for(int i= fila-1; i<=fila+1; i++) {
+				for(int j=columna-1; j<=columna; j++) {
+					if(this.tableroBack[i][j]==true) {
+						contadorDeMinas++;
+					}
+				}
+			}
+		}
+		if(fila == 0 && (columna>0 && columna < this.columnas-1)) {
+			for(int i= fila; i<=fila+1; i++) {
+				for(int j=columna-1; j<=columna+1; j++) {
+					if(tableroBack[i][j]==true) {
+						contadorDeMinas++;
+					}
+				}
+			}
+		}
+		if(fila == this.filas-1 && (columna>0 && columna < this.columnas-1)) {
+			for(int i= fila-1; i<=fila; i++) {
+				for(int j=columna-1; j<=columna+1; j++) {
+					if(tableroBack[i][j]==true) {
+						contadorDeMinas++;
+					}
+				}
+			}
+		}
+		if((fila>0 && fila<this.filas-1) && (columna>0 && columna<this.columnas-1)) {
+			for(int i= fila-1; i<=fila+1; i++) {
+				for(int j=columna-1; j<=columna+1; j++) {
+					if(tableroBack[i][j]==true) {
+						contadorDeMinas++;
+					}
+				}
+			}
+		}
+		
+		this.tableroFront[fila][columna] = (char)(contadorDeMinas+'0') ;
+		
+	}
+
+	
+	
+	public boolean consultarHayMinaEn(int fila, int columna) {
+		if(this.tableroBack[fila][columna] == true) {
+			return true;
+		}
+		return false;
+	}
+
+
+}
---
+++ test/ar/edu/unlam/pb2/BuscaminasMain.java
@@ -0,0 +1,132 @@
+package ar.edu.unlam.pb2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class BuscaminasMain {
+	private static boolean fueExitoso = true; 
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		Nivel nivel;
+		int opcion;
+		double casillero;
+		boolean inicio = true;
+		char continuar = 's';
+		int fila=0, columna=0;
+		boolean estaGanando = true;
+		
+		
+		do {
+			nivel = seleccionarNivel(teclado);
+			
+			Buscaminas buscaminas = new Buscaminas(nivel);
+			//------------------------------------------------
+			
+			
+			System.out.println(buscaminas.mostrarTableroFront());
+			
+			do {
+				
+				do {
+					try {
+						System.out.print("Seleccione un casillero, por ej: [fila=0 | columna=0] es 0,0: ");
+						casillero = teclado.nextDouble();
+						
+						String casilleroTexto = String.valueOf(casillero);
+						
+						fila = Integer.parseInt(casilleroTexto.substring(0, casilleroTexto.indexOf('.') ));
+						columna = Integer.parseInt(casilleroTexto.substring((casilleroTexto.indexOf('.')+1)));
+						
+						if(!(buscaminas.validarFilaColumna(fila, columna))) {
+							System.out.println("Ingresaste un valor incorrecto.Intente de nuevo");
+							fueExitoso = false;
+							
+						}else {
+							fueExitoso = true;
+						}	
+						
+					}catch(InputMismatchException exception) {
+						System.err.println("Error. Por favor ingrese un número decimal segun fila y columna corresponda. ");
+						fueExitoso = false;
+						teclado.next();
+					}
+					
+				}while(!fueExitoso);
+				
+				
+				
+				
+				if(inicio == true) {
+					buscaminas.generarLosCasillerosConMinas(fila, columna);
+					buscaminas.guardarLaCantidadDeBombasQueHayAlrededor(fila, columna);
+					inicio = false;
+				}else {
+					if(!buscaminas.consultarHayMinaEn(fila,columna)) {
+						buscaminas.guardarLaCantidadDeBombasQueHayAlrededor(fila, columna);
+					}else {
+						estaGanando = false;
+					}
+				}
+				
+				
+				System.out.println(buscaminas.mostrarTableroFront(estaGanando,fila, columna));
+				System.out.println(buscaminas.mostrarTableroBack());
+				
+				
+			}while(!buscaminas.consultarHayMinaEn(fila,columna));
+			System.out.println("Perdiste");
+			
+			
+			
+			//-----------FIN -------------------------
+			inicio = true;
+			estaGanando = true;
+			System.out.println("¿Desea continuar? (s/n)");
+			continuar = teclado.next().toLowerCase().charAt(0);
+			
+		}while(continuar == 's');
+		
+		
+
+	}
+
+	private static Nivel seleccionarNivel(Scanner teclado) {
+		Integer opcion = 0;
+		Nivel[]niveles = Nivel.values();
+		
+		do {
+			mostrarMenuDeNiveles(niveles);
+			try {
+				opcion = teclado.nextInt();
+				
+				if(opcion <= 0 || opcion > niveles.length) {
+					System.out.println("Opción incorrecta. Intente de nuevo.");
+					fueExitoso = false;
+				}else
+					fueExitoso = true;
+				
+			}catch(InputMismatchException exception) {
+				System.err.println("Ingresaste un caracter inválido, solo números por favor desde 1 hasta 3.");
+				fueExitoso = false;
+				teclado.next();
+			}
+			
+			
+			
+		}while(!fueExitoso);
+		
+		return niveles[opcion-1];
+	}
+
+	private static void mostrarMenuDeNiveles(Nivel[] niveles) {
+		System.out.println("\nNIVELES: ");
+		System.out.println("*************************");
+		for(int i=0; i<niveles.length; i++) {
+			System.out.println("["+(i+1)+"] "+ niveles[i]);	
+		}
+		System.out.print("\nSeleccione su nivel de juego [1-3]: ");		
+	}
+
+}
